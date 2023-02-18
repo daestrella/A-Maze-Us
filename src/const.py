eff15f5d70c542d30f7ae83a23ed1d5033109b14
David Andrei Estrella <102010106+daestrella@users.noreply.github.com>
2023-02-18 11:21:06 +0800

    version 2.0

--- src/const.py
+++ src/const.py
@@ -1,14 +1,15 @@
+# This contains constants used throughout the whole project
 # Pygame window dimensions
 W_WIDTH  = 700
 W_HEIGHT = 700
 
 # Window color scheme
-BLACK   = (29 , 53 , 87 )
-GREY    = (69 , 123, 157)
-WHITE   = (250, 243, 238)
-MACCENT = (230, 57 , 70 )
-SACCENT = (247, 166, 164)
+BLACK   = (40 , 45 , 60 )
+GREY    = (91 , 93 , 112)
+WHITE   = (255, 196, 184)
+MACCENT = (116, 131, 140)
+SACCENT = (246, 145, 151)
 
 # initial rows and columns
-INIT_ROWS = 5
-INIT_COLUMNS = 5
+INIT_ROWS = 20
+INIT_COLUMNS = 20
